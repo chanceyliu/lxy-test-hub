3afc83e48b1ceb52ea77ceb44ce860eeff74e86c
chanceyliu <chanceyliu@tencent.com>
2020-11-20 17:08:41 +0800

    init:构建react+ts模板

---
+++ react-hub/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Test() {
+  return (
+    <div>
+      <p></p>
+      <code>222</code>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
