231059ff5293eab2f637a41cb161acc3021c0af8
chanceyliu <1445885764@qq.com>
2021-10-08 11:20:36 +0800

    feat: 测试 react-img-contrast@2.0.0

--- src/pages/theme-color/index.tsx
+++ src/pages/theme-color/index.tsx
@@ -1,35 +1,33 @@
-import { FC, useEffect } from "react";
+import { FC, useCallback, useEffect } from "react";
 import defaultBackground from "../../assets/default-background.png";
 import defaultLoginBg from "../../assets/default-login-bg.png";
 import testLogin from "../../assets/test-login.jpeg";
-import { useImgContrast, useColorContrast } from "react-img-contrast";
-
-const divColor = "#eeeeee";
+import { getPalette, getColor, getTextColor } from "react-img-contrast";
 
 const Index: FC = () => {
-  const { imgContrast } = useImgContrast({
-    imgSrc: testLogin,
-    xMultiple: 0.3,
-    yMultiple: 0.8,
-    wMultiple: 0.4,
-    hMultiple: 0.2,
-  });
-  const { colorContrast } = useColorContrast(divColor);
+  const getImgPalette = useCallback(async () => {
+    const palette = await getPalette({ imgSrc: defaultLoginBg });
+    const color = await getColor({ imgSrc: defaultLoginBg });
+    const text = await getTextColor({ imgSrc: defaultLoginBg });
+    console.log(palette, "-", color, "-", text);
+  }, []);
+
+  useEffect(() => {
+    getImgPalette();
+  }, [getImgPalette]);
 
   return (
     <div
       style={{
         width: "100vw",
         height: "100vh",
-        backgroundImage: `url(${testLogin})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
       }}
     >
-      <div style={{ color: imgContrast }}>版权版权信息</div>
+      <h1>测试本地包</h1>
+      {/* <div style={{ color: imgContrast }}>版权版权信息</div>
       <div style={{ width: 300, height: 300, backgroundColor: divColor }}>
         <span style={{ color: colorContrast }}>文字颜色</span>
-      </div>
+      </div> */}
     </div>
   );
 };
